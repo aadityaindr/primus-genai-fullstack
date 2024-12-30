b550ad95e5c2e8414be0037202852b35ac74637b
aadityaraj <137440229+aadityaindr@users.noreply.github.com>
2024-12-30 10:17:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
   if (isMobile) {
     // Redirect or send a custom response for mobile devices
-    res.status(400).send('This website is not optimized for mobile devices.'); 
+    res.status(400).send('This website is not optimized for mobile devices. Kindly use a desktop, thanks!!'); 
     return;
   }
 
